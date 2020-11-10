3f1756be4e0eb3309f52824302014ff0d3749741
lasinicl <lasini.1021@gmail.com>
2020-11-10 12:08:39 +0530

    Fix merge conflicts

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/desugar/TransactionDesugar.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/desugar/TransactionDesugar.java
@@ -101,6 +101,7 @@ public class TransactionDesugar extends BLangNodeVisitor {
 
     private String uniqueId;
     private int transactionBlockCount;
+    private boolean transactionInternalModuleIncluded = false;
 
     private BLangStatementExpression result;
     private boolean onFailHandled;
@@ -307,8 +308,13 @@ public class TransactionDesugar extends BLangNodeVisitor {
     private BLangInvocation createStartTransactionInvocation(Location location,
                                                              BLangLiteral transactionBlockIDLiteral,
                                                              BLangSimpleVarRef prevAttempt) {
+        // Include transaction-internal module as an import if not included
+        if (!transactionInternalModuleIncluded) {
+            desugar.addTransactionInternalModuleImport();
+            transactionInternalModuleIncluded = true;
+        }
         BInvokableSymbol startTransactionInvokableSymbol =
-                (BInvokableSymbol) getTransactionLibInvokableSymbol(START_TRANSACTION);
+                (BInvokableSymbol) getInternalTransactionModuleInvokableSymbol(START_TRANSACTION);
         List<BLangExpression> args = new ArrayList<>();
         args.add(transactionBlockIDLiteral);
         args.add(prevAttempt);
@@ -373,7 +379,7 @@ public class TransactionDesugar extends BLangNodeVisitor {
     private BLangInvocation createCleanupTrxStmt(Location pos) {
         List<BLangExpression> args;
         BInvokableSymbol cleanupTrxInvokableSymbol =
-                (BInvokableSymbol) getTransactionLibInvokableSymbol(CLEAN_UP_TRANSACTION);
+                (BInvokableSymbol) getInternalTransactionModuleInvokableSymbol(CLEAN_UP_TRANSACTION);
         args = new ArrayList<>();
         args.add(transactionBlockID);
         BLangInvocation cleanupTrxInvocation = ASTBuilderUtil.
@@ -389,7 +395,7 @@ public class TransactionDesugar extends BLangNodeVisitor {
 
         // rollbackTransaction(transactionBlockID);
         BInvokableSymbol rollbackTransactionInvokableSymbol =
-                (BInvokableSymbol) getTransactionLibInvokableSymbol(ROLLBACK_TRANSACTION);
+                (BInvokableSymbol) getInternalTransactionModuleInvokableSymbol(ROLLBACK_TRANSACTION);
         List<BLangExpression> args = new ArrayList<>();
         args.add(transactionBlockID);
         if (rollbackNode.expr != null) {
@@ -427,7 +433,7 @@ public class TransactionDesugar extends BLangNodeVisitor {
         // Clear failures
         // boolean isFailed = getAndClearFailure();
         BInvokableSymbol transactionCleanerInvokableSymbol =
-                (BInvokableSymbol) getTransactionLibInvokableSymbol(GET_AND_CLEAR_FAILURE_TRANSACTION);
+                (BInvokableSymbol) getInternalTransactionModuleInvokableSymbol(GET_AND_CLEAR_FAILURE_TRANSACTION);
         BLangInvocation transactionCleanerInvocation = ASTBuilderUtil.
                 createInvocationExprForMethod(pos, transactionCleanerInvokableSymbol, new ArrayList<>(), symResolver);
         transactionCleanerInvocation.argExprs = new ArrayList<>();
@@ -445,7 +451,7 @@ public class TransactionDesugar extends BLangNodeVisitor {
         // Commit expr desugar implementation
         //string|error commitResult = endTransaction(transactionID, transactionBlockID);
         BInvokableSymbol commitTransactionInvokableSymbol =
-                (BInvokableSymbol) getTransactionLibInvokableSymbol(END_TRANSACTION);
+                (BInvokableSymbol) getInternalTransactionModuleInvokableSymbol(END_TRANSACTION);
         List<BLangExpression> args = new ArrayList<>();
         args.add(transactionID);
         args.add(transactionBlockID);
@@ -573,5 +579,15 @@ public class TransactionDesugar extends BLangNodeVisitor {
      */
     public BSymbol getTransactionLibInvokableSymbol(Name name) {
         return symTable.langTransactionModuleSymbol.scope.lookup(name).symbol;
+    }
+
+    /**
+     * Load and return symbol for given name in transaction internal module.
+     *
+     * @param name of the symbol.
+     * @return symbol for the function.
+     */
+    public BSymbol getInternalTransactionModuleInvokableSymbol(Name name) {
+        return symTable.internalTransactionModuleSymbol.scope.lookup(name).symbol;
     }
 }
